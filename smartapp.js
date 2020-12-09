2b3bf913303c1380b10b165540ff26ed959d9183
John Undersander <john.undersander@smartthings.com>
2020-12-09 14:21:12 -0600

    fix: try PORT env var first

--- smartapp.js
+++ smartapp.js
@@ -2,8 +2,8 @@ const SmartApp = require('@smartthings/smartapp');
 
 /* Define the SmartApp */
 module.exports = new SmartApp()
-    .enableEventLogging(2)  // logs requests and responses as pretty-printed JSON
-    .configureI18n()        // auto-create i18n files for localizing config pages
+    .enableEventLogging(2) // logs all lifecycle event requests/responses as pretty-printed JSON. Omit in production
+    .configureI18n() // auto-create i18n files for localizing config pages
 
     // Configuration page definition
     .page('mainPage', (context, page, configData) => {
